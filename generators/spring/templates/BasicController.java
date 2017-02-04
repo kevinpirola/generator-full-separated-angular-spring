16d54a505e7ee782cc45b9c1b6712f2086a88ae0
kevinpirola <gianmarco.laggia@gmail.com>
2017-02-04 17:20:41 +0100

    Fixed #5

--- generators/spring/templates/BasicController.java
+++ generators/spring/templates/BasicController.java
@@ -1,4 +1,4 @@
-package <%=packageName%>;
+package <%=packageName%>.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
